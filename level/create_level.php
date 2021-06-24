eb020e98df1c511ef286ac5dbc87aecb04fd69c7
Your Name <emiledennis47@gmail.com>
2021-06-24 07:32:06 +0000

    created level

---
+++ level/create_level.php
@@ -0,0 +1,38 @@
+<?php
+// Include config file
+require_once "../models/BaseModel.php";
+require '../functions.php';
+// Define variables and initialize with empty values
+$name = "";
+
+$msg = "";
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate name
+    $name = trim($_POST["name"]);
+    $data = ['name' => $name,];
+
+    $result = $model->insert('level', $data);
+    $msg = $result['message'];
+
+    //$result['id'] contains the last inserted id
+    //redirect("list_meeting.php");
+}
+?>
+
+<?= template_header('Create') ?>
+
+<div class="content">
+    <h2>Level</h2>
+    <form action=<?= $_SERVER['PHP_SELF']; ?> method="post">
+        <div>
+        <label for="name">Name</label>
+        <input type="text" name="name"  id="name" />
+        </div>
+        <input type="submit" value="Submit">
+    </form>
+    <p><?= $msg ?></p>
+
+</div>
+
+<?= template_footer() ?>
