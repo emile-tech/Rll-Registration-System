a751af9e2c69b722eb83a4f74937e768636a6d08
Your Name <emiledennis47@gmail.com>
2021-06-24 07:53:39 +0000

    update

---
+++ How_did_you_hear/edit_feedback.php
@@ -0,0 +1,50 @@
+<?php
+// Include config file
+require_once "../models/BaseModel.php";
+require '../functions.php';
+
+
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get hidden input value
+    $id = $_GET["id"];
+
+    // Validate name
+    $name = trim($_POST["name"]);
+    // Validate address address
+    $data = ['name' => $name,];
+    $result = $model->update('how_did_you_hear', $data);
+    $msg = $result['message'];
+
+    header('location: list_feedback.php');
+} else {
+    // Check existence of id parameter before processing further
+    if (isset($_GET["id"]) && !empty($_GET["id"])) {
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+
+        $result = $model->fetch('how_did_you_hear', $id);
+
+        $how_did_you_hear = $result['rows'];
+        // Retrieve individual field value
+        $name = $how_did_you_hear["name"];
+        
+    }
+}
+?>
+
+<?= template_header('Create') ?>
+
+<div class="content">
+    <h2>List of Feedback?</h2>
+    <form action=<?= $_SERVER['PHP_SELF']; ?> method="post">
+        <div>
+        <label for="name">Name</label>
+        <input type="text" name="name"  id="name" />
+        </div>
+        <input type="submit" value="Submit">
+    </form>
+    <p><?= $msg ?></p>
+
+</div>
